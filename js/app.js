fea32f27ab9be557d6842752279c52c00be28b8d
George Go <george.js.gosieski@gmail.com>
2016-02-12 15:54:28 -0800

    Prepared app.js with tests for query

--- js/app.js
+++ js/app.js
@@ -7,3 +7,18 @@
 // Test Case:
 // User only has potatoes
 // Provides Recipe for mashed potatoes
+var baseURL = "http://www.recipepuppy.com/api/?";
+var ingredients = "potato";
+var dish = "salad";
+var query = baseURL + "i=" + ingredients + "&q=" + dish;
+
+
+
+var req = new XMLHttpRequest();
+req.open('GET', query, false);
+req.send(null);
+if(req.status == 200) {
+   dump(req.responseText);
+}
+
+console.log(req.responseText);
