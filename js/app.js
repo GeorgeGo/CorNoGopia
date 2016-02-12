bb0511c131d0f36628cbd5cd64509fcd90140126
George Go <george.js.gosieski@gmail.com>
2016-02-12 15:44:47 -0800

    Updated todo, added plans in app.js, added jquery to vendors

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Connect to food api
+// Potential APIS
+//     https://developer.edamam.com/
+//     http://www.recipepuppy.com/about/api/
+
+
+// Test Case:
+// User only has potatoes
+// Provides Recipe for mashed potatoes
