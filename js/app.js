668391fd3440225d57141b098089850acaf45149
George Go <george.js.gosieski@gmail.com>
2016-02-14 09:45:05 -0800

    Working through api material, updated todo to reflect

--- js/app.js
+++ js/app.js
@@ -7,18 +7,29 @@
 // Test Case:
 // User only has potatoes
 // Provides Recipe for mashed potatoes
-var baseURL = "http://www.recipepuppy.com/api/?";
-var ingredients = "potato";
-var dish = "salad";
-var query = baseURL + "i=" + ingredients + "&q=" + dish;
 
+// food2fork
+const API_KEY = '5dbe8dc691de2b3d8db331019416a9e5';
 
+var website = 'http://food2fork.com/api/';
+// 'search' or 'get'
+var key = '?key='+API_KEY+'&q=';
+var search = 'potatoes';
+var query = website+'search'+key+search;
 
 var req = new XMLHttpRequest();
-req.open('GET', query, false);
-req.send(null);
+req.open('GET', query, true);
+req.send();
 if(req.status == 200) {
    dump(req.responseText);
 }
 
+console.log(req.status);
+console.log(req.statusText);
 console.log(req.responseText);
+
+
+// Send an XML request locally to a script in either rails or python (idk which)
+// Script processes request and calls relevant api for information
+
+// Test getJSON from webpage for x request
