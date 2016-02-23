3543d5220ab7c29eee0645adbedbc3381ea92f7e
George Go <george.js.gosieski@gmail.com>
2016-02-22 16:15:44 -0800

    needed to commit garbage

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/getRecipes',methods=['POST'])
+
+@app.route('/getRecipes', methods=['POST'])
 def getRecipes():
     try:
         # _food = request.form['food']
         # print _food
         _food = 'tomatoes'
         if _food:
-            options = {'key':'5dbe8dc691de2b3d8db331019416a9e5','q':_food}
-            r = requests.get('http://food2fork.com/api/search',params=options)
-            return (json.dumps(r.json()),200)
+            options = {'key': '5dbe8dc691de2b3d8db331019416a9e5', 'q': _food}
+            r = requests.get('http://food2fork.com/api/search', params=options)
+            return (json.dumps(r.json()), 200)
     except Exception as e:
-        return (json.dumps({'error':str(e)}),400)
+        return (json.dumps({'error': str(e)}), 400)
 
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // Connect to food api
 // Potential APIS
-//     https://developer.edamam.com/
-//     http://www.recipepuppy.com/about/api/
+//	 https://developer.edamam.com/
+//	 http://www.recipepuppy.com/about/api/
 
 
 // Test Case:
@@ -11,22 +11,22 @@
 // query = 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3';
 
 $(function () {
-    $('#getRecipes').click(function () {
-        $.ajax({
-            url: '/getRecipes',
-            type: 'POST',
-            data: {food:'tomatoes'},
-            success: function (response) {
-                var r = JSON.parse(response);
-                console.log(r['recipes'][0]);
-                for (var i = 0; i < r['count']; i++) {
-                    console.log(r['recipes'][i]['title']);
-                }
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
+	$('#getRecipes').click(function () {
+		$.ajax({
+			url: '/getRecipes',
+			type: 'POST',
+			data: {food:'tomatoes'},
+			success: function (response) {
+				var r = JSON.parse(response);
+				console.log(r['recipes'][0]);
+				for (var i = 0; i < r['count']; i++) {
+					console.log(r['recipes'][i]['title']);
+				}
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
 
-    });
+	});
 });
