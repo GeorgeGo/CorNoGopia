de77e6127c679f980b46cfafff4c350754791882
George Go <george.js.gosieski@gmail.com>
2016-03-19 14:31:43 -0700

    docs: Switched versions on todo
    feat: prepped server for call exceeded scenario

--- server.py
+++ server.py
@@ -3,8 +3,9 @@ import requests
 
 app = Flask(__name__)
 
-keys = ['5dbe8dc691de2b3d8db331019416a9e5',
-        '373a612eeeae2813e001680f04b585db']
+keys = ['373a612eeeae2813e001680f04b585db',
+        '5dbe8dc691de2b3d8db331019416a9e5']
+
 current_key_id = 0
 
 
@@ -28,8 +29,7 @@ def getRecipes():
             food = str(_food[0])
             for item in _food[1:]:
                 food+=','+item
-            r =
-            requests.get('http://food2fork.com/api/search'+'?key='+keys[current_key_id]+'&q='+food)
+            r = requests.get('http://food2fork.com/api/search'+'?key='+keys[current_key_id]+'&q='+food)
             return (json.dumps(r.json()), 200)
     # except Rate limit exceeded, change index and rerun
     except Exception as e:
@@ -40,12 +40,12 @@ def getRecipes():
 def getIngredients():
     try:
         _rid = request.form['rId']
-        options = {'key': key, 'rId': _rid}
+        options = {'key': keys[current_key_id], 'rId': _rid}
         r = requests.get('http://food2fork.com/api/get', params=options)
         return (json.dumps(r.json()), 200)
     except Exception as e:
         return (json.dumps({'error': str(e)}), 400)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
-    # app.run(port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(port=5000, debug=True)
