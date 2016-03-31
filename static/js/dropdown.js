c0a91f7da5068127e2a4ab5e219964e5bc5eb879
George Go <george.js.gosieski@gmail.com>
2016-03-30 17:21:09 -0700

    feat: added basic compress functionality
    compress not being executed on correct clicks
    expand not being called in conjunction
    dropdownLi providing unexpected behaviour with compress

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -7,22 +7,39 @@ function dropdownOn(){
 function dropdownOff(){
     $('html').on('click', function(){
         $('.number-li').hide();
-        compress();
     });
 }
 
 function compress(){
-    let a = $('#ingredientsForm input').children().length;
-    console.log(a);
+    var form = $('#ingredientsForm');
+    var formCount = form.children().length;
+    var items = form.serialize().split('&').map(function(item){
+        let i = item.indexOf('=');
+        return item.slice(i+1);
+    });
+    let compressValue = '';
+    for (var i=0;i<items.length-1;i++){
+        compressValue += items[i]+', ';
+    }
+    compressValue += items.slice(-1)[0];
+    form.empty(); 
+    var formEntry = $('<input type=\'text\' name=\'ingredient_0\' class=\'new-ingredient\' value=\''+compressValue+'\'>');
+    form.append(formEntry);
+    // compress should only be called if not clicking on dropdown or span
     // store contents of all inputs in array if non-empty
     // empty all but one input
     // store contents of array in first input
 }
 
+function expand(){
+    
+}
+
 function dropdownLi(){
     $('.number-li').on('click',function(e){
         $('.number-li').hide();
-
+        // Lengths here should be determined by span or integrate with
+        // expand/compress
         var previousLength = + $('.wrapper-dropdown .dropdown-label').text();
     	var newLength = + this.id[this.id.length-1];
         $('.wrapper-dropdown .dropdown-label').text(newLength);
