eff2086d4ded856071a00eba58b043aa35dfa875
George Go <george.js.gosieski@gmail.com>
2016-04-12 10:23:53 -0700

    fix: removed extraneous print statement

--- server.py
+++ server.py
@@ -51,11 +51,10 @@ def getIngredients():
     try:
         _rid = request.form['rId']
         r = checkKey('http://food2fork.com/api/get?rId='+_rid)
-        print r.url
         return (json.dumps(r.json()), 200)
     except Exception as e:
         return (json.dumps(str(e)), 400)
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000, debug=False)
+    # app.run(host='0.0.0.0', port=5000, debug=False)
     app.run(port=5000, debug=True)
